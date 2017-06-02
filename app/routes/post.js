d5821b09752d7efec1c3f62d1853f7d856fb27c0
Sara Schultz <saschultz95@gmail.com>
2017-06-02 13:12:58 -0700

    add functionality to update a post from the posts detail page

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    update(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          post.set(key,params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('index');
+    }
   }
 });
